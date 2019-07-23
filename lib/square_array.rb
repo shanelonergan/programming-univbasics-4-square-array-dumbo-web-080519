f9062562f3ebea1d13820b94c0929e022ed59efa
Shane Lonergan <sptlonergan@gmail.com>
2019-07-23 03:11:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,15 +1,18 @@
 
-#def square_array(array)
-#  counter = 0
-#  squared_array = []
-#  while array[counter] do
-#    element = array[counter] ** 2
-#    squared_array.push(element)
-#    counter += 1
-#  end
-#  return squared_array
-#end
-
-def square_array
-  square_array.map { [n] n ** 2 }
+def square_array(array)
+  counter = 0
+  squared_array = []
+  while array[counter] do
+    element = array[counter] ** 2
+    squared_array.push(element)
+    counter += 1
+  end
+  return squared_array
 end
+
+
+# Trying to streamline:
+
+#def square_array
+  #square_array.map { [n] n ** 2 }
+#end
